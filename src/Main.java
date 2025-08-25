b18c938bb0e930b5039333de85e3ac8a1b98f4aa
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-08-25 16:34:37 +0530

    Java New Course Initial

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello vandana");
+        int a=8;
+        int b=7;
+        System.out.println("The sum is: "+(a+b));
+    }
+}
+
+// best practices
+
+
+
+// white spaces not alowed
+//  45suman
+// sumn kumari
+
+// passcal convention --> SumanKumarGautam  
+// camel case convetion --> sumanKumarGautam
