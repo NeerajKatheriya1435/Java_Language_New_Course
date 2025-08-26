af839d5324f60e7700eb43577d7a1cff324880fa
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-08-26 16:35:26 +0530

    Notes added

---
+++ src/Day2.java
@@ -0,0 +1,45 @@
+public class Day2 {
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+////        int suman=8;
+//        double kritika=8.897;
+//         int sukoonkumari=89;
+////         int sukoonkumari=89;
+//        int suman; //declare
+//        suman=89; // initilize
+//
+////        int num1=78; // declaration with iitilization
+////        int bjsdbj=89;
+////        int Hbhjhbj$_=89;
+////        int num1=8;
+////        int Num1=7;
+////        System.out.println(num1);
+//    int num1=9;
+//    int num2=8;
+//    int sum=num1+num2;
+        byte num1=89;
+        short num2=78;
+        int num3=7890;
+        long num4=6897L;
+        float numFloat=56.896F;
+        Double numDouble=67576.8678;
+        char subham='(';
+        boolean ekValue=true;
+        String shiva="Suman Kumari";
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(numFloat);
+        System.out.println(numDouble);
+        System.out.println(subham);
+        System.out.println(shiva);
+        System.out.println(ekValue);
+        int for123=90;
+
+        int[] arr={6,8,9,};
+//        for (int item :arr){
+//            System.out.println(item);
+//        }
+    }
+}
---
+++ src/Day2New.java
@@ -0,0 +1,20 @@
+public class Day2New {
+    public static void main(String[] args) {
+//        int num1=6;
+//        int num2=9;
+//        System.out.println(num1+num2);
+//        System.out.println(num1-num2);
+//        System.out.println(num1*num2);
+//        System.out.println(num1/num2);
+
+        // This my Calculator
+        // jksdfb
+//        gftgh
+//        fjkdgjdf
+     /*   multi
+                line
+                commet
+      */
+    }
+
+}
