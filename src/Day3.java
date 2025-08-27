ee7d8a8da3916c4d566aa91ee2ad5c9dad492a80
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-08-27 16:37:24 +0530

    userinput and operator

---
+++ src/Day3.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class Day3 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+//        int val1=sc.nextInt();
+//
+//        float val2=sc.nextFloat();
+//        System.out.println("You entered value "+val1);
+//        System.out.println("You entered value "+val2);
+
+//        String str1=sc.next();
+//        String str1=sc.nextLine();
+//        System.out.println("The value in str1 is "+str1);
+
+//        String nameStr="Hardik";
+//        String str2="Pandya";
+//        int age=78;
+//        System.out.println(nameStr+" "+str2+" "+age);
+//        System.out.println("Enter the number");
+//        int age=sc.nextInt();
+//        System.out.println(age);
+//        System.out.println("Plaese enter temp in cel:");
+//        float celciuos=sc.nextFloat();
+//        float foren=(celciuos * 9/5) + 32;
+//        System.out.println("The temp in foren is: "+foren);
+
+        int a=12;
+        int b=7;
+        int c=12;
+
+        // Arithmetic operator
+//        System.out.println("The sum is: "+(a+b));
+//        System.out.println("The mul is: "+(a*b));
+//        System.out.println("The div is: "+((float)a/b));
+//        System.out.println("The sub is: "+(a-b));
+//        System.out.println("The mod is: "+(a%b));
+
+        // Relational Operator
+
+//        System.out.println(a==b); // a=12 b=7 c=12  //false
+//        System.out.println(a==c);  //true
+//        System.out.println(a==b);
+//        System.out.println(a>b);  // true
+//        System.out.println(a<b);  //false
+//        System.out.println(a<=b); //false
+//        System.out.println(a>=b); //true
+//        System.out.println(a>=c); //true
+//        System.out.println(a<=c); //true
+//        System.out.println(a!=c);
+
+//        System.out.println(a>b && a>c); //false
+//        System.out.println(a<b && a>c); //false
+//        System.out.println(a>b && a>=c && a==c);// true
+//        System.out.println(a<b || a>c); // false
+//        System.out.println(a<b || a<c ||a==c); //true
+
+        // assigment operator
+//        int sum=3;
+//        int num1=sum+7;
+//        sum=sum+7;
+//        int num1=5;
+//        num1+=7; //num1=num1+7
+//        num1-=3;
+//        num1*=2;
+//        System.out.println(num1);
+
+        // unary operator
+        int num1=6;
+//        num1++; //num1=num1+1
+//        System.out.println(++num1);  //7  //7
+//        System.out.println(num1--); //6  //7
+//        System.out.println(num1); // 6   //6
+//        System.out.println(++num1); // 7  //7
+//        System.out.println(++num1); // 8  //8
+//        System.out.println(num1--); // 7  //8
+//        System.out.println(num1++); // 8  // 7
+//        System.out.println(num1); // 8  //8
+//        System.out.println(--num1); // 7  //7
+//        System.out.println(!(7>9));
+//        System.out.println(2&3);
+//        System.out.println(2|3);
+//        String result = (age >= 18) ? "Adult" : "Minor";
+//        int mark=17;
+
+        // ternay operator
+//        String marks=(mark>50)?"Good Marks":"Tum Nalayak Ho";
+//        System.out.println(marks);
+    }
+}
