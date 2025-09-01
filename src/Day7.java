3d17f7469a1431cd286c87d5bfaae53edbe7b855
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-09-01 16:45:03 +0530

    String Methods and nested loop

---
+++ src/Day7.java
@@ -0,0 +1,59 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Day7 {
+    public static void main(String[] args) {
+//        String str1="VanDAna Kumari Khushi Khushi";
+//        String str2="VanDAna";
+//        String str3="VandAna";
+//        String str2="Khushi";
+//        System.out.println(str1+" "+str2);
+//        System.out.println(str1.length());
+//        System.out.println(str1.toLowerCase());
+//        System.out.println(str1.toUpperCase());
+//        System.out.println(str1.charAt(3));
+//        System.out.println(str1.charAt(13));
+//        System.out.println(str1.substring(9,12));
+//        System.out.println(str1.substring(0,4));
+//        System.out.println("Khushi".equals("KhushI"));
+//        System.out.println(str2.equalsIgnoreCase(str3));
+//        System.out.println(str1.contains("Kh"));
+//        System.out.println(str1.replace("Khushi","Alok"));
+        String str1="   bfg ";
+//        System.out.println(str1.trim());
+//        System.out.println(str1.startsWith("Khu"));
+//        System.out.println(str1.endsWith("a"));
+//        System.out.println(str1.indexOf("A"));
+//        System.out.println(str1.indexOf("iu"));
+//        System.out.println(str1.indexOf("ax",4));
+//        System.out.println(str1.lastIndexOf("a"));
+//        System.out.println(str1.isEmpty());
+//        System.out.println(str1.isBlank());
+//        System.out.print("Shhivam ");
+//        System.out.print("Khushi");
+
+//        *******
+//        *******
+//        *******
+//        *******
+
+//        for (int i = 0; i <5; i++) {
+//            for (int j = 0; j <=i; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of row: ");
+        int n=sc.nextInt();
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <n-i ; j++) {
+                System.out.print("  ");
+            }1
+            for (int k = 0; k <2*i-1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
