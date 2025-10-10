a4fc7e6e6463d5bee7ad52746b183376d3fd5fa8
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2025-10-10 17:37:21 +0530

    Refactor code

---
+++ DSA_Class_Code/src/DSADay1.java
@@ -0,0 +1,11 @@
+public class DSADay1 {
+    public static void main(String[] args) {
+        System.out.println(5&3);
+        System.out.println(5|3);
+        System.out.println(5^3);
+        int a=5;
+//        System.out.println(Integer.toBinaryString(~a));
+//        System.out.println(a<<1);
+        System.out.println(a>>1);
+    }
+}
---
+++ DSA_Class_Code/src/Delete1.java
@@ -0,0 +1,39 @@
+public class Delete1 {
+    //Problem 1:  Write a program to sum 1 to n Numbers
+
+    // 1st Method
+    public static int FirstMethod(int n){
+
+        int sum=0;
+        for (int i = 1; i <=n ; i++) {
+            sum+=i;
+        }
+        return sum;
+    }
+
+    // 2nd Method
+    public static void SecondMethodSum(int n){
+        int result=(n*(n+1))/2;
+        System.out.println(result);
+    }
+
+    // Problem 2: A1 executes in 1 Second and A2 Executes in 2 Seconds (Find Best)
+    // Exmaple of (2GB File at Friend's Home) --> Depends on Language, Processing, Logic
+    // Depends on Number of Operations (A is Best if it best on every machine and language)
+    // Exmample Find Stolen phone in 5 students (Best/Average/Worst Case)
+
+//    Exmaple 1: Travesring an array of N length
+
+//    Exmaple 2: Travesring an array of N length and M length individually
+//    Exmaple 3: Travesring an nested array of N length and M length inside N arr[n][m]
+//    Exmaple 3: Travesring an nested array of N length and M length inside N arr[n][n-1]
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
